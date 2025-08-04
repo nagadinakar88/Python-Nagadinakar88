1e9109ce342ea71fb017b01140550b45886563e7
nagadinakar88 <nagadinakar88@gmail.com>
2025-08-04 06:12:01 +0530

    Printing class of data types

---
+++ 01_Vars_data_types/myntra.py
@@ -0,0 +1,10 @@
+brand_name ='UNited Colours of Benetton'
+product_Description = 'Men Chest Pocket Slim Fit Casual Shirt'
+product_rating = 3.8
+product_price = 1651
+product_sizes =[38,40,42]
+print(type(product_sizes))
+print(type(brand_name))
+print(type(product_Description))
+print(type(product_rating))
+print(type(product_price))
